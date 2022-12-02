88a02c195ce90aab9ee0fadf509373bb493ab3f4
miecz <miecz@LAPTOP-UVM3H6HN>
2022-12-02 10:05:10 +0100

    Add project files.

---
+++ ProjectCosmo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ProjectCosmo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert input");
+            string userInput = Console.ReadLine();
+
+            SubString(userInput);
+            Replace(userInput);
+        }
+
+        static void SubString(string userInput)
+        {
+            if (userInput.Length > 10)
+            {
+                string startSubstring = userInput.Substring(0, 10);
+                string endSubstring = userInput.Substring(userInput.Length - 10);
+                Console.WriteLine($"{startSubstring}....,...{endSubstring}");
+                Console.WriteLine(userInput);
+            }
+        }
+
+        static void Replace(string userInput)
+        {
+
+        }
+    }
+}
