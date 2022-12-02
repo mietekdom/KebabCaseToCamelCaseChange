208d645fa785b967987d08b36832f06bfce58b3e
miecz <miecz@LAPTOP-UVM3H6HN>
2022-12-02 10:18:37 +0100

    working on string

--- ProjectCosmo/Program.cs
+++ ProjectCosmo/Program.cs
@@ -9,8 +9,10 @@ namespace ProjectCosmo
             Console.WriteLine("Insert input");
             string userInput = Console.ReadLine();
 
-            SubString(userInput);
-            Replace(userInput);
+            //SubString(userInput);
+            //Replace(userInput);
+            //Modify(userInput);
+            // AlterTextCase(userInput);
         }
 
         static void SubString(string userInput)
@@ -23,8 +25,25 @@ namespace ProjectCosmo
                 Console.WriteLine(userInput);
             }
         }
-
         static void Replace(string userInput)
+        {
+            string template = "Hello {name}, how are you?";
+            string output = template.Replace("{name}", userInput);
+            Console.WriteLine(output);
+        }
+        static void Modify(string userInput)
+        {
+            string removedString = userInput.Remove(5);
+            string subString = userInput.Substring(5);
+            string insertString = userInput.Insert(5, "Samolot");
+            string trimedString = userInput.Trim();
+
+            Console.WriteLine(removedString);
+            Console.WriteLine(subString);
+            Console.WriteLine(insertString);
+            Console.WriteLine(trimedString);
+        }
+        static void AlterTextCase()
         {
 
         }
